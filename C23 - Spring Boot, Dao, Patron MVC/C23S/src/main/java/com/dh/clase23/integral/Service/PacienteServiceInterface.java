2d2cd60d5c1ee6435ae4f457bb0ed056dad7fbc7
EliasPaladino <eliaspaladino@outlook.com>
2022-11-25 00:36:39 -0300

    Fix services

--- C23 - Spring Boot, Dao, Patron MVC/C23S/src/main/java/com/dh/clase23/integral/Service/PacienteServiceInterface.java
+++
@@ -1,9 +0,0 @@
-package com.dh.clase23.integral.Service;
-
-import com.dh.clase23.integral.dominio.Paciente;
-
-import java.util.List;
-
-public interface PacienteServiceInterface {
-    List<Paciente>
-}
