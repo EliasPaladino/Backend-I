4d6af82ef99c688c1f209184e44869f94c197199
EliasPaladino <eliaspaladino@outlook.com>
2023-01-16 11:23:57 -0300

    Terminamos index.html y creamos create.sql

--- integrador/src/main/java/com/dh/clase23/integrador/IntegradorApplication.java
+++ integrador/src/main/java/com/dh/clase23/integrador/IntegradorApplication.java
@@ -3,8 +3,31 @@ package com.dh.clase23.integrador;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 @SpringBootApplication
 public class IntegradorApplication {
+
+	private static void cargarBD(){
+		Connection connection = null;
+
+		try {
+
+			Class.forName("org.h2.Driver").newInstance();
+			connection = DriverManager.getConnection("jdbc:h2:~/integrador;INIT=RUNSCRIPT FROM 'create.sql'", "sa", "sa");
+
+		} catch ( Exception e ) {
+			e.printStackTrace();
+		} finally {
+			try {
+				connection.close();
+			} catch ( SQLException ex ) {
+				ex.printStackTrace();
+			}
+		}
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(IntegradorApplication.class, args);
--- integrador/src/main/java/com/dh/clase23/integrador/controller/PacienteController.java
+++ integrador/src/main/java/com/dh/clase23/integrador/controller/PacienteController.java
@@ -2,13 +2,17 @@ package com.dh.clase23.integrador.controller;
 
 import com.dh.clase23.integrador.dominio.Paciente;
 import com.dh.clase23.integrador.service.PacienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
 public class PacienteController {
     private final PacienteService pacienteService;
 
+    @Autowired
     public PacienteController(PacienteService pacienteService) {
         this.pacienteService = pacienteService;
     }
