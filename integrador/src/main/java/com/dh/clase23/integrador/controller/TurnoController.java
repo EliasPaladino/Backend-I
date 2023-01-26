2ff0c482f8e8ef963de2483667590c5285913249
EliasPaladino <eliaspaladino@outlook.com>
2023-01-26 09:41:18 -0300

    Agrego listarTurnos en el controller

--- integrador/src/main/java/com/dh/clase23/integrador/controller/TurnoController.java
+++ integrador/src/main/java/com/dh/clase23/integrador/controller/TurnoController.java
@@ -1,4 +1,26 @@
 package com.dh.clase23.integrador.controller;
 
+import com.dh.clase23.integrador.dominio.Turno;
+import com.dh.clase23.integrador.service.TurnoService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/turnos")
 public class TurnoController {
+
+    private final TurnoService turnoService;
+
+    public TurnoController(TurnoService turnoService) {
+        this.turnoService = turnoService;
+    }
+
+    @GetMapping
+    public List<Turno> listarTurnos() {
+        return turnoService.listarTurnos();
+    }
+
 }
